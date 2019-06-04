1611c12d91e7036cde251675fb99f35be5070722
TheGingeraffe <37917265+TheGingeraffe@users.noreply.github.com>
2019-06-03 19:08:32 -0700

    Delete TicketAction.py

--- TicketAction.py
+++
@@ -1,8 +0,0 @@
-# class AddTicket(argparse.Action):
-    # def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        # if nargs is not None:
-            # raise ValueError("nargs not allowed")
-            # super(AddTicket, self).__init__(option_strings, dest, **kwargs)
-    # def __call__(self, parser, namespace, values, option_string=None):
-        # print('%r %r %r' % (namespace, values, option_string))
-        # setattr(namespace, self.dest, values)
